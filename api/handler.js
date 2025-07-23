06c032731a4d46d173a374aa55b21223e1374c3c
eldudeeeeeee <artinteificiallligence9249@gmail.com>
2025-07-24 01:22:03 +0300

    Update handler.js

--- api/handler.js
+++ api/handler.js
@@ -1,18 +1,4 @@
-const express = require("express");
-const serverless = require("serverless-http");
-
-const app = express();
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.post("/", (req, res) => {
-  console.log("📩 Incoming request");
-  console.log("🔍 Headers:", req.headers);
-  console.log("📦 Body:", req.body);
-
-  res.status(200).send("✅ Slack handler received your request!");
-});
-
-module.exports = app;
-module.exports.handler = serverless(app);
+export default async function handler(req, res) {
+  console.log("Incoming request:", req.method, req.body);
+  res.status(200).json({ message: "Slack handler is running ✅" });
+}
